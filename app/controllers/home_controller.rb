5950f3fd8c3533851ec146661471b8d65eea9e38
diegolagreca <dvazquezlagreca@gmail.com>
2015-01-02 18:29:37 +0000

    nueva version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   
   def index
       @titulo = "Home - Dashboard de Incidentes"
+      @incidentes = Incidente.all
   end
   def about
     @titulo = "About - Dashboard de Incidentes"
---
+++ db/migrate/20150102014309_create_incidentes.rb
@@ -0,0 +1,14 @@
+class CreateIncidentes < ActiveRecord::Migration
+  def change
+    create_table :incidentes do |t|
+      t.string :asunto
+      t.string :descripcion
+      t.string :responsable
+      t.string :tipo
+      t.integer :prioridad
+      
+      t.timestamps
+    end
+  end
+end
+
