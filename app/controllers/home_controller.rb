d9f5fa7146ae45e25136d3a7b1f56a55db3e0c10
diegolagreca <dvazquezlagreca@gmail.com>
2015-01-02 01:04:17 +0000

    Avanzando segun curso udemy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
+  
   def index
+      @titulo = "Home - Dashboard de Incidentes"
   end
   def about
+    @titulo = "About - Dashboard de Incidentes"
   end
 end
